5f17535455e4ef695091bd802730ad272355a444
BT-Kyaw <bhonethantk12@gmail.com>
2024-05-20 07:19:43 +0630

    Episode-3-Global-Object

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello A Legend");
+// global.setTimeout(() => {
+//     console.log("Hello");
+// }, 3000);
+
+let interval = global.setInterval(() => {
+    console.log("Hello World");
+}, 1000);
+
+global.setTimeout(() => {
+    clearInterval(interval)
+}, 3000);
+
+console.log(__dirname);
+console.log(__filename);
