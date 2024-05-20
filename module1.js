fb7b1409e4e567db050257e755001bf8e725164b
BT-Kyaw <bhonethantk12@gmail.com>
2024-05-20 07:32:52 +0630

    Episode-4-Node-Module-System

---
+++ module1.js
@@ -0,0 +1,9 @@
+const myName = "BhoneThant-Kyaw";
+const myAge = 23;
+const life = "kmn";
+
+module.exports = {
+   myName,
+   myAge,
+   life
+};
---
+++ module2.js
@@ -0,0 +1,3 @@
+let {myName,myAge,life} = require('./module1');
+
+console.log(life);
